a6b1157b79a23e8366d5b474abc06f60c4dd9eef
JazMatt <mat.jazdzewski@outlook.com>
2024-03-03 22:16:51 +0100

    Added function to read description text from file

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 import express from "express";
+import fs from "fs/promises";
 
 const port = 3000;
 const app = express();
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home.ejs");
-})
+app.get("/", async (req, res) => {
+
+  // Getting offer description from file
+  let desc = "";
+  try {
+    desc = (await readDescFromFile());
+    desc = desc.replace(/\n/g, "<br>");
+  } catch (err) {
+    console.error("Error reading file:", err);
+    desc = "😊";
+  }
+  
+  res.render("home.ejs", { description: desc });
+});
 
 app.listen(port, () => {
   console.log("Server running on port " + port);
-})
+});
+
+async function readDescFromFile() {
+  try {
+    const data = await fs.readFile('public/resources/desc.txt', 'utf8');
+    return data;
+  } catch (err) {
+    console.error("Error reading file:", err);
+    return "";
+  }
+}
